1637130446c6efeee091c1c9a241fb6e86e35006
DebjeetDam <debjeet.dam95@gmail.com>
2024-05-04 20:27:58 -0500

    testing FizzBuzz

--- src/tests/intro.test.js
+++ src/tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { max } from "../intro";
+import { fizzBuzz, max } from "../intro";
 
 //to create test
 // Arrange
@@ -9,6 +9,8 @@ import { max } from "../intro";
 // const result = max(a, b);
 // Assert
 // expect(result).toBe(2);
+
+// Suite 1
 describe("max", () => {
   it("should return first arg if it is greater", () => {
     expect(max(2, 1)).toBe(2);
@@ -18,5 +20,20 @@ describe("max", () => {
   });
   it("should return first arg if it is equal", () => {
     expect(max(1, 1)).toBe(1);
+  });
+});
+// Suite 2
+describe("fizzbuzz", () => {
+  it("should return FizzBuzz if arg is div by 3 & 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+  it("should return Fizz if arg is div by 3", () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+  });
+  it("should return Buzz if arg is div by 5", () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+  });
+  it("should return arg as str if not div by 3 or 5", () => {
+    expect(fizzBuzz(2)).toBe("2");
   });
 });
