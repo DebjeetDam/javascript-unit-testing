409e02ad275f9c33b60de003e26008f87bea924e
DebjeetDam <debjeet.dam95@gmail.com>
2024-05-07 23:26:06 -0500

    async test

--- src/tests/core.test.js
+++ src/tests/core.test.js
@@ -2,6 +2,7 @@ import { it, expect, describe, expectTypeOf } from "vitest";
 import {
   calculateDiscount,
   canDrive,
+  fetchData,
   getCoupons,
   isPriceInRange,
   isValidUsername,
@@ -132,5 +133,16 @@ describe("canDrive", () => {
     { age: 18, country: "UK", result: true },
   ])("should return $result for $age, $country", ({ age, country, result }) => {
     expect(canDrive(age, country)).toBe(result);
+  });
+});
+
+describe("fetchData", () => {
+  it("should ret a promise -> resolve to an arr", async () => {
+    try {
+      const result = await fetchData();
+    } catch (error) {
+      expect(error).toHaveProperty("reason");
+      expect(error.reason).toMatch(/fail/i);
+    }
   });
 });
